c4df8a1e42faf0eb1d3e9e74b9fba4e4b96b29d5
maximilianludwig <41318163+maximilianludwig@users.noreply.github.com>
2020-04-30 16:13:20 +0200

    Update test.groovy

--- jenkins/test.groovy
+++ jenkins/test.groovy
@@ -1,11 +1,15 @@
-pipeline{
-  agent any
-  stages{
-    stage("test"){
-      steps{
-        echo "test"
-      }
+pipeline {
+    agent any
+    stages {
+        stage('Example') {
+            steps {
+                echo 'Hello World'
+            }
+        }
     }
-  }
+    post { 
+        always { 
+            echo 'I will always say Hello again!'
+        }
+    }
 }
-   
